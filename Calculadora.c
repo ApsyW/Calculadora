8bc07ee3c065a9ccee9c2bdcadf551443b536cc0
AdrianAS <arellano.sosa.adrian@gmail.com>
2024-02-20 22:17:52 -0600

    Crear las entradas donde se van a pedir los números

---
+++ Calculadora.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    char operador;
+    double num1, num2, resultado;
+
+    printf("Ingrese el primer número: ");
+    scanf("%lf", &num1);
+    printf("Ingrese el operador (+, -, *, /): ");
+    scanf(" %c", &operador);
+    printf("Ingrese el segundo número: ");
+    scanf("%lf", &num2);
+
+    
+
+    return 0;
+}
